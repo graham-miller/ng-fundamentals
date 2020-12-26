f5419b2eedaf018229a29ddd4685a989f4d26250
Graham Miller <mail@grahammiller.net>
2020-12-26 20:29:24 +0000

    Chapter 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,55 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {
+  EventsListComponent,
+  EventThumbnailComponent,
+  EventService,
+  EventDetailsComponent,
+  CreateEventsComponent,
+} from './events/index';
+
 import { EventsAppComponent } from './events-app.component';
-import { EventsListComponent } from './events/events-list.component';
-import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavBarComponent } from './nav/navbar.component';
-import { EventService } from './events/shared/event.service';
 import { ToastrService } from './common/toastr.service';
+import { RouterModule } from '@angular/router';
+import { appRoutes } from './routes';
+import { Error404Component } from './errors/404.component';
+import { EventRouteActivator } from './events/event-details/event-route-activator-service';
+import { EventsListResolver } from './events/events-list-resolver.service';
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   declarations: [
     EventsAppComponent,
     EventsListComponent,
     EventThumbnailComponent,
-    NavBarComponent
+    NavBarComponent,
+    EventDetailsComponent,
+    CreateEventsComponent,
+    Error404Component
   ],
   providers: [
     EventService,
-    ToastrService
+    ToastrService,
+    EventRouteActivator,
+    {
+      provide: 'canDeactivateCreateEvent',
+      useValue: checkDirtyState
+    },
+    EventsListResolver
   ],
   bootstrap: [EventsAppComponent]
 })
+
 export class AppModule { }
+
+export function checkDirtyState(component:CreateEventsComponent) {
+  if (component.isDirty)
+    return window.confirm('You have not saved this component, do you really want to cancel?');
+
+  return true;
+}
---
+++ src/app/events/create-events.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+    template: `
+        <h1>New event</h1>
+        <hr>
+        <div class="col-md-6">
+            <h3>[Form here]</h3>
+            <br />
+            <br />
+            <button type="submit" class="btn btn-primary">
+                Save
+            </button>&nbsp;
+            <button
+                type="button" class="btn btn-default"
+                (click)="cancel()">Cancel</button>
+        </div>
+    `
+})
+export class CreateEventsComponent {
+
+    isDirty:boolean = true;
+
+    constructor(private router: Router) { }
+
+    cancel() {
+        this.router.navigate(['/events']);
+    }
+
+}
